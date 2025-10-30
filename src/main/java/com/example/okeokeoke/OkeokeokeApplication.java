5ee59a839745e7bb58c041b426a0a3d37e23819f
Lieu Nhat Minh <nhminemore@gmail.com>
2025-10-30 16:51:56 +0700

    test add+commit all

---
+++ src/main/java/com/example/okeokeoke/OkeokeokeApplication.java
@@ -0,0 +1,13 @@
+package com.example.okeokeoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkeokeokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkeokeokeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/okeokeoke/OkeokeokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.okeokeoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkeokeokeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
